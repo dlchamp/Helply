0a5aa9b1a744ce9c87e547073d6b6f3ce1191530
DLCHAMP <36091350+dlchamp@users.noreply.github.com>
2023-09-07 18:02:17 -0500

    docs: add docstrings to new Localized types

--- src/helply/types/localized.py
+++ src/helply/types/localized.py
@@ -16,14 +16,52 @@ __all__ = (
 
 
 class LocalizedArgument(ArgumentBase):
-    """Represents a slash command argument with localized attributes"""
+    """Represents a slash command argument with localized attributes
+
+    Attributes
+    ----------
+    name: str
+        Argument's non-localized name
+    description: str
+        Argument's non-localized description
+    required: bool
+        Whether or not the argument is required
+    """
 
     def __init__(self, name: str, description: str, required: bool) -> None:
         super().__init__(name=name, description=description, required=required)
 
 
 class LocalizedAppCommand(AppCommandBase):
-    """Represents an AppCommand with localized attributes"""
+    """Represents an AppCommand with localized attributes
+
+    Attributes
+    ----------
+    id : int
+        The command's unique identifier.
+    name: str
+        Command's name
+    description: str
+        Command's description
+    name_localizations: disnake.LocalizationValue
+        Contains localizations for the command's name. (*New in version 0.3.0*)
+    checks : CommandChecks
+        The command's permission and role requirements.
+    type: AppCommandType
+        Type of command
+    category: str
+        Name of cog or category the command belongs to
+    dm_permission : bool
+        Whether the command is available in DMs or not.
+    nsfw : bool
+        Whether the command is NSFW (Not Safe For Work).
+    guild_id : Optional[int]
+        The ID of the guild where the command is available.
+    default_member_permissions : Permissions, optional
+        Default member permissions required to use this command.
+    mention : str
+        Get the command as a mentionable if slash command, else returns bolded name.
+    """
 
     def __init__(
         self,
