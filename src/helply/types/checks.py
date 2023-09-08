cce97b169320f6f15548f1e018e787014da16585
DLCHAMP <36091350+dlchamp@users.noreply.github.com>
2023-09-08 10:06:56 -0500

    feat(cooldowns): add cooldown object

--- src/helply/types/checks.py
+++ src/helply/types/checks.py
@@ -1,9 +1,12 @@
-import typing
+from typing import List, NamedTuple, Union
 
-__all__ = ("CommandChecks",)
+__all__ = (
+    "CommandChecks",
+    "Cooldown",
+)
 
 
-class CommandChecks(typing.NamedTuple):
+class CommandChecks(NamedTuple):
     """Wrap the command's permission or role requirements.
 
     Attributes
@@ -14,5 +17,41 @@ class CommandChecks(typing.NamedTuple):
         A list of role names or role IDs required to use this command.
     """
 
-    permissions: typing.List[str]
-    roles: typing.List[typing.Union[str, int]]
+    permissions: List[str]
+    roles: List[Union[str, int]]
+
+
+class Cooldown(NamedTuple):
+    """Represents a commands.Cooldown
+
+    (*New in version: 0.4.0*)
+
+    Attributes
+    ----------
+    rate: int
+        Number of times the command can be used before triggering a cooldown
+    per: float
+        Amount of seconds to wait for a cooldown when it's been triggered
+    type: str
+        Type of cooldown
+
+
+
+    ```
+    |--------------------------------------------------------------------|
+    |   Types    |                   Description                         |
+    |------------|-------------------------------------------------------|
+    | `default`  | The default bucket operates on a global basis.        |
+    | `user`     | The user bucket operates on a per-user basis.         |
+    | `guild`    | The guild bucket operates on a per-guild basis.       |
+    | `channel`  | The channel bucket operates on a per-channel basis.   |
+    | `member`   | The member bucket operates on a per-member basis.     |
+    | `category` | The category bucket operates on a per-category basis. |
+    | `role`     | The role bucket operates on a per-role basis.         |
+    |--------------------------------------------------------------------|
+    ```
+    """
+
+    rate: int
+    per: float
+    type: str
