859dbdaa69acc3ccba8d86322d7e38325df97433
DLCHAMP <contact@dlchamp.com>
2024-08-27 21:15:42 -0500

    organize utils

--- src/helply/utils/__init__.py
+++ src/helply/utils/__init__.py
@@ -1,9 +1,6 @@
 """Provide some utility functions for creating embeds and a Paginator."""
+
 from .embeds import command_detail_embed, commands_overview_embeds
 from .paginator import Paginator
 
-__all__ = (
-    "command_detail_embed",
-    "commands_overview_embeds",
-    "Paginator",
-)
+__all__ = ("command_detail_embed", "commands_overview_embeds", "Paginator")
